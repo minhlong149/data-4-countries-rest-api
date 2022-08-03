3ee4cb9f0b49e7929f2bdeced60c93b5bad9c6a1
minhlong149 <minhlong9514@gmail.com>
2022-08-03 22:15:21 +0700

    Styling country detail

--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -1,8 +1,65 @@
-export default function CountryDetail({ viewDetail, updateView }) {
+export default function CountryDetail({ country, updateCountry }) {
+  console.log(Object.entries(country.languages).map((c) => c[1]));
+
+  const officialNativeName = Object.entries(country.name.nativeName).map(
+    (c) => c[1].official
+  );
+  const currenciesName = Object.entries(country.currencies).map(
+    (c) => c[1].name
+  );
+  const languages = Object.entries(country.languages).map((c) => c[1]);
   return (
-    <div>
-      <p>{viewDetail.name.common}</p>
-      <button onClick={() => updateView(undefined)}>Go Back</button>;
+    <div className="text-dark-text p-12">
+      <button
+        className="flex items-center gap-2 bg-dark-element rounded-md py-2 px-6 mb-8"
+        onClick={() => updateCountry(undefined)}
+      >
+        <span className="material-icons text-base">arrow_back</span>Back
+      </button>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-x-12 gap-y-6">
+        <img
+          className="rounded-md"
+          src={country.flags.svg}
+          alt={country.name.common + " flag"}
+        />
+
+        <div className="">
+          <h1 className="text-2xl font-bold pb-4">{country.name.common}</h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <p>
+                <b>Native Name:</b> {officialNativeName.join(", ")}
+              </p>
+              <p>
+                <b>Population:</b> {country.population.toLocaleString()}{" "}
+              </p>
+              <p>
+                <b>Region:</b> {country.region}
+              </p>
+              <p>
+                <b>Sub Region:</b> {country.subregion}
+              </p>
+              <p>
+                <b>Capital:</b> {country.capital?.join(", ") || "None"}{" "}
+              </p>
+            </div>
+
+            <div>
+              <p>
+                <b>Top-level domain:</b> {country.tld?.join(", ") || "None"}
+              </p>
+              <p>
+                <b>Currencies:</b> {currenciesName.join(", ")}
+              </p>
+              <p>
+                <b>Languages:</b> {languages.join(", ")}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
