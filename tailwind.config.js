a5e35d6cdb4c41949dc20ff2297535a36f2e0ba2
minhlong149 <minhlong9514@gmail.com>
2022-08-03 11:35:00 +0700

    Adding Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-text": " hsl(0, 0%, 100%)",
+        "dark-background": "hsl(207, 26%, 17%)",
+        "dark-element": "hsl(209, 23%, 22%)",
+        "light-text": " hsl(200, 15%, 8%)",
+        "light-background": "hsl(0, 0%, 98%)",
+        "light-element": "hsl(0, 0%, 100%)",
+        "light-input": " sl(0, 0%, 52%))",
+      },
+      fontSize: {
+        "homepage": "16px",
+        "detail": "14px",
+      },
+      fontFamily: {
+        sans: ["Nunito Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
